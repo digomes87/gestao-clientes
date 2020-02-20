ba173889fbebc709ac33d49a4c8700b179fef013
Diego G <diego.gomes87@gmail.com>
2020-02-20 17:18:19 -0300

    add templates

---
+++ clientes/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import persons_list
+from .views import persons_new
+from .views import persons_update
+from .views import persons_delete
+from .views import person_index
+
+
+urlpatterns = [
+    path('', person_index, name="person_index"),
+    path('list/', persons_list, name="persons_list"),
+    path('new/', persons_new, name="persons_new"),
+    path('update/<int:id>/', persons_update, name="persons_update"),
+    path('delete/<int:id>', persons_delete, name="persons_delete")
+]
