441160c6a9e17238452638c677b8adef12739b47
Diego G <diego.gomes87@gmail.com>
2020-02-20 13:19:34 -0300

    remove the last project and start again

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gestao-clientes
+
+Vamos lá aqui irei postar todo o passo a passo para rodar o projeto.
+Keep Calm !
--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -1,7 +1,7 @@
 """gestao_clientes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from clientes import urls as clientes_urls
-from home import urls as home_urls
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-
+from django.urls import path
 
 urlpatterns = [
-    path('', include(home_urls)),
-    path('clientes/', include(clientes_urls)),
-    path('login/', auth_views.login, name='login'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]
